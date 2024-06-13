ffeda1723c657bc47fe1e29639aa4b79ca0a8b5d
nicolas-prates <nicolas.prates@sptech.school>
2024-06-13 11:01:53 -0300

    permite o envio do email do proprio usuario no update

--- src/main/java/com/qualaboa/msauth/services/UserService.java
+++ src/main/java/com/qualaboa/msauth/services/UserService.java
@@ -108,7 +108,9 @@ public class UserService implements IServiceSave<UserCreateDTO, UserResponseDTO>
     public UserResponseDTO update(UserUpdateDTO request, UUID uuid) {
         User entity = repository.findById(uuid).orElseThrow(()
                 -> new ResourceNotFoundException("Resource not found"));
-        if(request.getEmail() != null && repository.existsByEmail(request.getEmail())) throw new ResourceNotFoundException("Email already exists");
+        if(request.getEmail() != null
+            && !request.getEmail().equals(entity.getEmail())
+            && repository.existsByEmail(request.getEmail())) throw new ResourceNotFoundException("Email already exists");
         entity = UserMapper.toEntity(request, entity);
 
         entity.setPassword(passwordEncoder.encode(entity.getPassword()));
